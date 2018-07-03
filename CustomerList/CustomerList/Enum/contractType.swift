3ce1ab61ed2b8b31ff9e85211f0091289bb4b7e2
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-07-04 00:44:15 +0200

    Added 2 new enums

---
+++ CustomerList/CustomerList/Enum/contractType.swift
@@ -0,0 +1,15 @@
+//
+//  contractType.swift
+//  CustomerList
+//
+//  Created by Ahmed Elbasha on 7/4/18.
+//  Copyright © 2018 Ahmed Elbasha. All rights reserved.
+//
+
+import Foundation
+
+enum ContractType : String {
+    case rent = "للآيجار"
+    case buy = "شراء"
+    case sell = "بيع"
+}
---
+++ CustomerList/CustomerList/Enum/finishingType.swift
@@ -0,0 +1,17 @@
+//
+//  finishingType.swift
+//  CustomerList
+//
+//  Created by Ahmed Elbasha on 7/4/18.
+//  Copyright © 2018 Ahmed Elbasha. All rights reserved.
+//
+
+import Foundation
+
+enum FinishingType: String {
+    case onTheBone = "على العضم"
+    case onTurbinate = "على المحارة"
+    case lux = "لوكس"
+    case superLux = "سوبر لوكس"
+    case ultraLux = "الترا لوكس"
+}
