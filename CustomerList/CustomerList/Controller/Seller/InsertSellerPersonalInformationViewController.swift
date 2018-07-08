54809d93b59561093600cf414b30de2129d16af9
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-07-08 18:20:05 +0200

    InsertSellerPersonalInformationViewController is wired up

--- CustomerList/CustomerList/Controller/Seller/InsertSellerPersonalInformationViewController.swift
+++ CustomerList/CustomerList/Controller/Seller/InsertSellerPersonalInformationViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class InsertSellerPersonalInformationViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var sellerNameTextField: UITextField!
+    @IBOutlet weak var nationalityTextField: UITextField!
+    @IBOutlet weak var birthDayDatePicker: UIDatePicker!
+    @IBOutlet weak var femaleButton: UIButton!
+    @IBOutlet weak var maleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,20 +27,15 @@ class InsertSellerPersonalInformationViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     @IBAction func backButtonWasPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func femaleButtonWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func maleButtonWasPressed(_ sender: Any) {
     }
     
 }
