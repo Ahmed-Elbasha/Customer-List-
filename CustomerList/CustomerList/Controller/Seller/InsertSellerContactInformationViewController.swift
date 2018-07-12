ef49e8362d52c4602e37ac3fddf68ef078e555bf
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-07-12 20:51:28 +0200

    Add Logic to InsertSellerContactInformationViewController class

--- CustomerList/CustomerList/Controller/Seller/InsertSellerContactInformationViewController.swift
+++ CustomerList/CustomerList/Controller/Seller/InsertSellerContactInformationViewController.swift
@@ -63,117 +63,87 @@ class InsertSellerContactInformationViewController: UIViewController, UITextFiel
         firstAddressLineTextField.delegate = self
         secondAddressLineTextField.delegate = self
     }
-
-    @IBAction func backButtonWasPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
     
-    @IBAction func createSellerWasPressed(_ sender: Any) {
-        if isDataValid() {
-            // Saving Seller Data to CoreData xcmodeldata.
-            let confirmationAlert = UIAlertController(title: "Success", message: "انت على وشك آضافة بيانات جديدة هل انت متآكد؟", preferredStyle: .alert)
-            
-            confirmationAlert.addAction(UIAlertAction(title: "نعم", style: .default, handler: { (alert) in
-                self.saveSeller { (completed) in
-                    
-                    if completed {
-                        
-                        let successAlert = UIAlertController(title: "نجاح العملية", message: "تم ادخال البيانات بنجاح", preferredStyle: .alert)
-                        successAlert.addAction(UIAlertAction(title: "حسنا", style: .default, handler: { (action) in
-                            let sellerViewController: SellerViewController = self.storyboard?.instantiateViewController(withIdentifier: "SellerViewController") as! SellerViewController
-                            self.dismiss(animated: true, completion: nil)
-                            self.present(sellerViewController, animated: true, completion: nil)
-                        }))
-                        self.present(successAlert, animated: true, completion: nil)
-                        
-                    } else {
-                        let failureAlert = UIAlertController(title: "فشل العملية", message: "فشل اثناء التنفيذ", preferredStyle: .alert)
-                        failureAlert.addAction(UIAlertAction(title: "حسنا", style: .default, handler: { (alert) in
-                            return
-                        }))
-                        self.present(failureAlert, animated: true, completion: nil)
-                    }
-                }
-            }))
-            
-            confirmationAlert.addAction(UIAlertAction(title: "الغاء التنفيد", style: .cancel, handler: { (alert) in
-                return
-            }))
-            
-            self.present(confirmationAlert, animated: true, completion: nil)
-            
+    func setClassValues() {
+        if isAllDataValid() {
+            setAllClassData()
+        } else if isRequiredDataValid() {
+            setRequiredClassData()
         } else {
-            let errorAlert: UIAlertController = UIAlertController(title: "خطآ", message: "يبدو انه  هناك خطآ حدث اثناء ادخال البيانات رجائا قم بمراجعة بياناتك ", preferredStyle: .alert)
-            errorAlert.addAction(UIAlertAction(title: "حسنا", style: .cancel, handler: { (alert) in
-                return
-            }))
-            self.present(errorAlert, animated: true, completion: nil)
+            showErrorAlertController()
         }
     }
     
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        textField.clearButtonMode = .whileEditing
-        return ((textField.text != "") || (!(textField.text?.isEmpty)!))
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        switch textField.tag {
-        case 0:
-            if textField.text != "" || textField.text?.isEmpty != true {
-                phoneLineNumber = Int32(phoneLineNumberTextField.text!)!
-            }
-            break
-        case 1:
-            if textField.text != "" || textField.text?.isEmpty != true {
-                firstMobileNumber = Int32(firstMobileNumberTextField.text!)!
-            }
-            break
-        case 2:
-            if textField.text != "" || textField.text?.isEmpty != true {
-                secondMobileNumber = Int32(secondMobileNumberTextField.text!)!
-            }
-            break
-        case 3:
-            if textField.text != "" || textField.text?.isEmpty != true {
-                whatsNumber = Int32(whatsNumberTestField.text!)!
-            }
-            break
-        case 4:
-            if textField.text != "" || textField.text?.isEmpty != true {
-                firstEmailAddress = firstEmailAddressTextField.text!
-            }
-            break
-        case 5:
-            if textField.text != "" || textField.text?.isEmpty != true {
-                secondEmailAddress = secondEmailAddressTextField.text!
-            }
-            break
-        case 6:
-            if textField.text != "" || textField.text?.isEmpty != true {
-                firstAddressLine = firstAddressLineTextField.text!
-            }
-            break
-        case 7:
-            if textField.text != "" || textField.text?.isEmpty != true {
-                secondAddressLine = secondAddressLineTextField.text!
-            }
-            break
-        default:
-            break
-        }
-        textField.resignFirstResponder()
-    }
-    
-    func isDataValid() ->Bool {
-        if (phoneLineNumber == 0 || firstMobileNumber == 0 || secondMobileNumber == 0 || whatsNumber == 0 || firstEmailAddress == "" || secondEmailAddress == "" || firstAddressLine == "" || secondAddressLine == "") {
+    func isAllDataValid() ->Bool {
+        if (phoneLineNumberTextField.text != "0" || firstMobileNumberTextField.text != "0" || secondMobileNumberTextField.text != "0" || whatsNumberTestField.text != "0" || firstEmailAddressTextField.text != "" || secondEmailAddressTextField.text != "" || firstAddressLineTextField.text != "" || secondAddressLineTextField.text != "") {
+            return true
+        } else {
             return false
+        }
+    }
+    
+    func isRequiredDataValid() -> Bool {
+        if (phoneLineNumberTextField.text != "0" || firstMobileNumberTextField.text != "0" || whatsNumberTestField.text != "0" || firstEmailAddressTextField.text != "" || firstAddressLineTextField.text != "") {
+            return true
         } else {
             return true
+        }
+    }
+    
+    func showErrorAlertController() {
+        let errorAlert: UIAlertController = UIAlertController(title: "خطآ", message: "يبدو انه  هناك خطآ حدث اثناء ادخال البيانات رجائا قم بمراجعة بياناتك ", preferredStyle: .alert)
+        
+        errorAlert.addAction(UIAlertAction(title: "حسنا", style: .cancel, handler: { (alert) in
+            return
+        }))
+        
+        self.present(errorAlert, animated: true, completion: nil)
+    }
+    
+    func setAllClassData() {
+        phoneLineNumber = Int32(phoneLineNumberTextField.text!)!
+        firstMobileNumber = Int32(firstMobileNumberTextField.text!)!
+        secondMobileNumber = Int32(secondMobileNumberTextField.text!)!
+        whatsNumber = Int32(whatsNumberTestField.text!)!
+        firstEmailAddress = firstEmailAddressTextField.text!
+        secondEmailAddress = secondEmailAddressTextField.text!
+        firstAddressLine = firstAddressLineTextField.text!
+        secondAddressLine = secondAddressLineTextField.text!
+    }
+    
+    func setRequiredClassData() {
+        phoneLineNumber = Int32(phoneLineNumberTextField.text!)!
+        firstMobileNumber = Int32(firstMobileNumberTextField.text!)!
+        secondMobileNumber = 0
+        whatsNumber = Int32(whatsNumberTestField.text!)!
+        firstEmailAddress = firstEmailAddressTextField.text!
+        secondEmailAddress = ""
+        firstAddressLine = firstAddressLineTextField.text!
+        secondAddressLine = ""
+    }
+    
+    func showConfirmationAlertController() {
+        // Saving Seller Data to CoreData xcmodeldata.
+        let confirmationAlert = UIAlertController(title: "Success", message: "انت على وشك آضافة بيانات جديدة هل انت متآكد؟", preferredStyle: .alert)
+        
+        confirmationAlert.addAction(UIAlertAction(title: "نعم", style: .default, handler: { (alert) in
+            self.InitiateSavingSellerDataOperation()
+        }))
+        
+        confirmationAlert.addAction(UIAlertAction(title: "الغاء التنفيد", style: .cancel, handler: { (alert) in
+            return
+        }))
+        
+        self.present(confirmationAlert, animated: true, completion: nil)
+    }
+    
+    func InitiateSavingSellerDataOperation() {
+        self.saveSeller { (completed) in
+            if completed {
+                showOperationSuccessAlertController()
+            } else {
+                showOperationFailureAlertController()
+            }
         }
     }
     
@@ -203,4 +173,52 @@ class InsertSellerContactInformationViewController: UIViewController, UITextFiel
         }
     }
     
+    func showOperationSuccessAlertController() {
+        let successAlert = UIAlertController(title: "نجاح العملية", message: "تم ادخال البيانات بنجاح", preferredStyle: .alert)
+        successAlert.addAction(UIAlertAction(title: "حسنا", style: .default, handler: { (action) in
+            self.moveBackToMainScene()
+        }))
+        self.present(successAlert, animated: true, completion: nil)
+    }
+    
+    func moveBackToMainScene() {
+        let InsertSellerPersonalInformationVC: InsertSellerPersonalInformationViewController = self.storyboard?.instantiateViewController(withIdentifier: "InsertSellerPersonalInformationViewController") as! InsertSellerPersonalInformationViewController
+        self.present(InsertSellerPersonalInformationVC, animated: false, completion: nil)
+        InsertSellerPersonalInformationVC.dismiss(animated: true, completion: nil)
+    }
+    
+    func showOperationFailureAlertController() {
+        let failureAlert = UIAlertController(title: "فشل العملية", message: "فشل اثناء التنفيذ", preferredStyle: .alert)
+        failureAlert.addAction(UIAlertAction(title: "حسنا", style: .default, handler: { (alert) in
+            return
+        }))
+        self.present(failureAlert, animated: true, completion: nil)
+    }
+    
+    func confirmSavingSellerDataOperation() {
+        showConfirmationAlertController()
+    }
+    
+    @IBAction func backButtonWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createSellerWasPressed(_ sender: Any) {
+        setClassValues()
+        showConfirmationAlertController()
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        textField.clearButtonMode = .whileEditing
+        return (textField.text != "")
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.resignFirstResponder()
+    }
 }
