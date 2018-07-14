6cae06b9659d73785f719814b5aaa90206045b5c
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-07-14 13:48:58 +0200

    Added Logic to RealEstateTableViewCell class

--- CustomerList/CustomerList/View/RealEstateTableViewCell.swift
+++ CustomerList/CustomerList/View/RealEstateTableViewCell.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class RealEstateTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var areaNameLabel: UILabel!
+    @IBOutlet weak var compoundNameLabel: UILabel!
+    @IBOutlet weak var ownerNameLabel: UILabel!
+    @IBOutlet weak var stageNumberLabel: UILabel!
+    @IBOutlet weak var floorNumberLabel: UILabel!
+    @IBOutlet weak var buildingNumberLabel: UILabel!
+    @IBOutlet weak var unitNumberLabel: UILabel!
+    @IBOutlet weak var viewLabel: UILabel!
+    @IBOutlet weak var numberOfElevatorsLabel: UILabel!
+    @IBOutlet weak var numberOfBalconiesLabel: UILabel!
+    @IBOutlet weak var numberOfRoomsLabel: UILabel!
+    @IBOutlet weak var numberOfMasterRoomsLabel: UILabel!
+    @IBOutlet weak var numberOfDressingRoomsLabel: UILabel!
+    @IBOutlet weak var numberOfBathroomsLabel: UILabel!
+    @IBOutlet weak var numberOfPiecesOfReceptionsLabel: UILabel!
+    @IBOutlet weak var landAreaLabel: UILabel!
+    @IBOutlet weak var buildingAreaLabel: UILabel!
+    @IBOutlet weak var gardenAreaLabel: UILabel!
+    @IBOutlet weak var contractTypeLabel: UILabel!
+    @IBOutlet weak var apartmentTypeLabel: UILabel!
+    @IBOutlet weak var finishingTypeLabel: UILabel!
+    @IBOutlet weak var constructionDateLabel: UILabel!
+    @IBOutlet weak var deliveryDateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -17,8 +41,33 @@ class RealEstateTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
 
+    func configureCell(apartment: Apartment) {
+        areaNameLabel.text = apartment.areaName
+        compoundNameLabel.text = apartment.compoundName
+        ownerNameLabel.text = apartment.ownerName
+        stageNumberLabel.text = String(describing: apartment.stageNumber)
+        floorNumberLabel.text = String(describing: apartment.floorNumber)
+        buildingNumberLabel.text = String(describing: apartment.buildingNumber)
+        unitNumberLabel.text = String(describing: apartment.unitNumber)
+        viewLabel.text = apartment.view
+        numberOfElevatorsLabel.text = String(describing: apartment.numberOfElevators)
+        numberOfBalconiesLabel.text = String(describing: apartment.numberOfBalconies)
+        numberOfRoomsLabel.text = String(describing: apartment.numberOfRooms)
+        numberOfMasterRoomsLabel.text = String(describing: apartment.numberOfMasterRooms)
+        numberOfDressingRoomsLabel.text = String(describing: apartment.numberOfDressingRooms)
+        numberOfBathroomsLabel.text = String(describing: apartment.numberOfBathrooms)
+        numberOfPiecesOfReceptionsLabel.text = String(describing: apartment.numberOfPiecesOfReception)
+        landAreaLabel.text = String(describing: apartment.landArea)
+        buildingAreaLabel.text = String(describing: apartment.buildingArea)
+        gardenAreaLabel.text = String(describing: apartment.gardenArea)
+        contractTypeLabel.text = apartment.contractType
+        apartmentTypeLabel.text = apartment.apartmentType
+        finishingTypeLabel.text = apartment.finishingType
+        constructionDateLabel.text = apartment.constructionYear
+        deliveryDateLabel.text = apartment.deliveryYear
+    }
+    
 }
