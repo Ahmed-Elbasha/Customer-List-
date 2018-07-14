1e37154be5addcbd875299339495349340c7f166
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-07-14 12:41:45 +0200

    Added Logic to BuyerTableViewCell class

--- CustomerList/CustomerList/View/BuyerTableViewCell.swift
+++ CustomerList/CustomerList/View/BuyerTableViewCell.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class BuyerTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var buyerNameLabel: UILabel!
+    @IBOutlet weak var buyerNationalityLabel: UILabel!
+    @IBOutlet weak var buyerBirthDateLabel: UILabel!
+    @IBOutlet weak var buyerGenderLabel: UILabel!
+    @IBOutlet weak var numberOfFamilyMembersLabel: UILabel!
+    @IBOutlet weak var numberOfKidsLabel: UILabel!
+    @IBOutlet weak var maritalStatusLabel: UILabel!
+    @IBOutlet weak var phoneLineNumberLabel: UILabel!
+    @IBOutlet weak var firstMobileNumberLabel: UILabel!
+    @IBOutlet weak var secondMobileNumberLabel: UILabel!
+    @IBOutlet weak var whatsNumberLabel: UILabel!
+    @IBOutlet weak var firstEmailAddressLabel: UILabel!
+    @IBOutlet weak var secondEmailAddressLabel: UILabel!
+    @IBOutlet weak var firstAddressLineLabel: UILabel!
+    @IBOutlet weak var secondAddressLineLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,5 +36,23 @@ class BuyerTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+    func configureCell(buyer: Buyer) {
+        buyerNameLabel.text = buyer.buyerName
+        buyerNationalityLabel.text = buyer.nationality
+        buyerBirthDateLabel.text = buyer.birthdate
+        buyerGenderLabel.text = buyer.gender
+        numberOfFamilyMembersLabel.text = String(describing: buyer.familyMembers)
+        numberOfKidsLabel.text = String(describing: buyer.numberOfKids)
+        maritalStatusLabel.text = buyer.maritalStatus
+        phoneLineNumberLabel.text = String(describing: buyer.telephoneNumber)
+        firstMobileNumberLabel.text = String(describing: buyer.mobileNumber1)
+        secondMobileNumberLabel.text = String(describing: buyer.mobileNumber2)
+        whatsNumberLabel.text = String(describing: buyer.watsapNumber1)
+        firstEmailAddressLabel.text = buyer.emailAddress1
+        secondEmailAddressLabel.text = buyer.emailAddress2
+        firstAddressLineLabel.text = buyer.address1
+        secondAddressLineLabel.text = buyer.address2
+    }
+    
 }
