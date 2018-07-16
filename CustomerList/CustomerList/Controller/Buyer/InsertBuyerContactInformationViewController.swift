9e3e15ce030ecb78031cad2bbafd6ea7f678fa03
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-07-16 18:21:52 +0200

    Cleaned Up InsertBuyerContactInformationViewController class's code

--- CustomerList/CustomerList/Controller/Buyer/InsertBuyerContactInformationViewController.swift
+++ CustomerList/CustomerList/Controller/Buyer/InsertBuyerContactInformationViewController.swift
@@ -11,6 +11,7 @@ import CoreData
 
 class InsertBuyerContactInformationViewController: UIViewController {
     
+    // MARK: IBOutlets
     @IBOutlet weak var phoneLineNumberTextField: UITextField!
     @IBOutlet weak var firstMobileNumberTextField: UITextField!
     @IBOutlet weak var secondMobileNumberTextField: UITextField!
@@ -20,6 +21,7 @@ class InsertBuyerContactInformationViewController: UIViewController {
     @IBOutlet weak var firstAddressLineTextField: UITextField!
     @IBOutlet weak var secondAddressLineTextField: UITextField!
     
+    // MARK: Class's Attributes
     var phoneLineNumber: Int32 = 0
     var firstMobileNumber: Int32 = 0
     var secondMobileNumber: Int32 = 0
@@ -36,11 +38,13 @@ class InsertBuyerContactInformationViewController: UIViewController {
     var numberOfKids: Int32 = 0
     var maritalStatus: String = ""
 
+    // MARK: Application Life Cycle Methods.
     override func viewDidLoad() {
         super.viewDidLoad()
-        setDelegateForUIControls()
+        self.setDelegateForUIControls()
     }
     
+    // MARK: ViewController's Received Data Setup
     func initWithData(name: String, nationality: String, birthdate: String, gender: String, numberOfFamilyMembers: Int32, numberOfKids: Int32, maritalStatus: String) {
         self.buyerName = name
         self.buyerNationality = nationality
@@ -51,164 +55,14 @@ class InsertBuyerContactInformationViewController: UIViewController {
         self.maritalStatus = maritalStatus
     }
     
-    func isAllDataValid() -> Bool {
-        if phoneLineNumberTextField.text != "0" && firstMobileNumberTextField.text != "0" && secondMobileNumberTextField.text != "0" && whatsappNumberTextField.text != "0" && firstEmailAddressTextField.text != "" && secondEmailAddressTextField.text != "" && firstAddressLineTextField.text != "" && secondAddressLineTextField.text != "" {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    func setAllClassData() {
-        phoneLineNumber = Int32(phoneLineNumberTextField.text!)!
-        firstMobileNumber = Int32(firstMobileNumberTextField.text!)!
-        secondMobileNumber = Int32(secondMobileNumberTextField.text!)!
-        whatsappNumber = Int32(whatsappNumberTextField.text!)!
-        firstEmailAddress = firstEmailAddressTextField.text!
-        secondEmailAddress = secondEmailAddressTextField.text!
-        firstAddressLine = firstAddressLineTextField.text!
-        secondAddressLine = secondAddressLineTextField.text!
-    }
-    
-    func isRequiredDataIsValid() -> Bool {
-        if phoneLineNumberTextField.text != "0" && firstMobileNumberTextField.text != "0" && whatsappNumberTextField.text != "0" && firstEmailAddressTextField.text != "" && firstAddressLineTextField.text != "" {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    func setRequiredClassData() {
-        phoneLineNumber = Int32(phoneLineNumberTextField.text!)!
-        firstMobileNumber = Int32(firstMobileNumberTextField.text!)!
-        secondMobileNumber = 0
-        whatsappNumber = Int32(whatsappNumberTextField.text!)!
-        firstEmailAddress = firstEmailAddressTextField.text!
-        secondEmailAddress = ""
-        firstAddressLine = firstAddressLineTextField.text!
-        secondAddressLine = ""
-    }
-    
-    func checkTheValidationOfData() {
-        if isAllDataValid() {
-            setAllClassData()
-        } else if isRequiredDataIsValid() {
-            setRequiredClassData()
-        } else {
-            showErrorAlertController()
-        }
-    }
-    
-    func showErrorAlertController() {
-        let errorAlert: UIAlertController = UIAlertController(title: "خطآ", message: "يبدو انه  هناك خطآ حدث اثناء ادخال البيانات رجائا قم بمراجعة بياناتك ", preferredStyle: .alert)
-        
-        errorAlert.addAction(UIAlertAction(title: "حسنا", style: .cancel, handler: { (alert) in
-            return
-        }))
-        
-        self.present(errorAlert, animated: true, completion: nil)
-    }
-    
-    func confiremTheInitiationOfSavingBuyerDataOperation() {
-        showConfirmationAlertController()
-    }
-    
-    func initiateSavingBuyerDataOperation() {
-        saveBuyerData { (completed) in
-            if completed {
-                self.showOperationSuccessAlertController()
-            } else {
-                self.showOperationFailureAlertController()
-            }
-        }
-    }
-    
-    func showOperationSuccessAlertController() {
-        let successAlert = UIAlertController(title: "نجاح العملية", message: "تم ادخال البيانات بنجاح", preferredStyle: .alert)
-        successAlert.addAction(UIAlertAction(title: "حسنا", style: .default, handler: { (action) in
-            self.moveBackToMainScene()
-        }))
-        self.present(successAlert, animated: true, completion: nil)
-    }
-    
-    func showOperationFailureAlertController() {
-    let failureAlert = UIAlertController(title: "فشل العملية", message: "فشل اثناء التنفيذ", preferredStyle: .alert)
-    failureAlert.addAction(UIAlertAction(title: "حسنا", style: .default, handler: { (alert) in
-    return
-    }))
-    self.present(failureAlert, animated: true, completion: nil)
-    }
-    
-    func moveBackToMainScene() {
-        let insertBuyerPersonalInformationViewController = storyboard?.instantiateViewController(withIdentifier: "InsertBuyerContactInformationViewController") as! InsertBuyerContactInformationViewController
-        self.present(insertBuyerPersonalInformationViewController, animated: true, completion: nil)
-        insertBuyerPersonalInformationViewController.dismiss(animated: true, completion: nil)
-    }
-    
-    func saveBuyerData(completion: (_ completed: Bool) -> () ) {
-        let managedContext: NSManagedObjectContext? =  appDelegate.persistentContainer.viewContext
-        let buyer = Buyer(context: managedContext!)
-        
-        buyerName = buyer.buyerName!
-        buyerNationality = buyer.nationality!
-        birthdate = buyer.birthdate!
-        gender = buyer.gender!
-        numberOfFamilyMembers = buyer.familyMembers
-        numberOfKids = buyer.numberOfKids
-        maritalStatus = buyer.maritalStatus!
-        phoneLineNumber = buyer.telephoneNumber
-        firstMobileNumber = buyer.mobileNumber1
-        secondMobileNumber = buyer.mobileNumber2
-        whatsappNumber = buyer.watsapNumber1
-        firstEmailAddress = buyer.emailAddress1!
-        secondEmailAddress = buyer.emailAddress2!
-        firstAddressLine = buyer.address1!
-        secondAddressLine = buyer.address2!
-        
-        do {
-            try managedContext?.save()
-            print("Data Saved Successfully.")
-            completion(true)
-        } catch {
-            print("Data Saving Operation Failed" + error.localizedDescription)
-            completion(false)
-        }
-    }
-    
-    func showConfirmationAlertController() {
-            // Saving Seller Data to CoreData xcmodeldata.
-            let confirmationAlert = UIAlertController(title: "Success", message: "انت على وشك آضافة بيانات جديدة هل انت متآكد؟", preferredStyle: .alert)
-            
-            confirmationAlert.addAction(UIAlertAction(title: "نعم", style: .default, handler: { (alert) in
-                self.initiateSavingBuyerDataOperation()
-            }))
-            
-            confirmationAlert.addAction(UIAlertAction(title: "الغاء التنفيد", style: .cancel, handler: { (alert) in
-                return
-            }))
-            
-            self.present(confirmationAlert, animated: true, completion: nil)
-    }
-    
-    func setDelegateForUIControls() {
-        phoneLineNumberTextField.delegate = self
-        firstMobileNumberTextField.delegate = self
-        secondMobileNumberTextField.delegate = self
-        whatsappNumberTextField.delegate = self
-        firstEmailAddressTextField.delegate = self
-        secondEmailAddressTextField.delegate = self
-        firstAddressLineTextField.delegate = self
-        secondAddressLineTextField.delegate = self
-    }
-    
+    // MARK: IBActions
     @IBAction func backButtonWasPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-    
     @IBAction func addBuyerButtonpressed(_ sender: Any) {
-        checkTheValidationOfData()
-        confiremTheInitiationOfSavingBuyerDataOperation()
+        self.checkTheValidationOfData()
+        self.confiremTheInitiationOfSavingBuyerDataOperation()
     }
     
 }
